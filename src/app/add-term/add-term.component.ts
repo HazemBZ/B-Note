9656ef836f5299e42c4426c458d6382a59ab1668
HazemBZ <hazembenbz@gmail.com>
2020-07-27 16:01:46 +0100

    UI changes:   -add page: restructered html and added css

--- src/app/add-term/add-term.component.ts
+++ src/app/add-term/add-term.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { TermServiceService } from '../term-service.service';
 import { Term } from '../term';
+import { faSave } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add-term',
   templateUrl: './add-term.component.html',
-  styleUrls: ['./add-term.component.css']
+  styleUrls: ['./add-term.component.css'],
+  host: {'class': 'add tile is-11'}
 })
 export class AddTermComponent implements OnInit {
   term:Term;
+  faSave = faSave;
   constructor(private termService:TermServiceService) { }
 
   ngOnInit(): void {
