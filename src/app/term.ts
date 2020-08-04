a3a3c5521b43ac829d5eed4bf4f672938d2c9df6
HazemBZ <hazembenbz@gmail.com>
2020-08-04 23:56:26 +0100

    Data type update:   -term: contains a list of link objects which map link aliases and link values
    UI update:
      -Search Page: reflects new data type intended behavior (show an alias that links to a web page)

--- src/app/term.ts
+++ src/app/term.ts
@@ -1,8 +1,12 @@
 
+type link = {
+    alias: string,
+    value: string
+}
 export interface Term {
     _id:string
     term:string
     desc:string
     tags:string[]
-    links:string[]
+    links:link[]
 }
